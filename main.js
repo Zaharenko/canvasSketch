c596b469aafaf44d2b8b5f05dde06a3f16815ab5
azakharenka <anton.zaharenko@gmail.com>
2024-04-06 15:12:01 +0300

    Drawing a Skewed Rectangle

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
     dimensions: [1080, 1080],
 };
 
 const sketch = () => {
+    let x, y, w, h;
+    let radius, angle, rx, ry;
+
     return ({ context, width, height }) => {
-        let x, y, w, h;
         context.fillStyle = "white";
         context.fillRect(0, 0, width, height);
 
@@ -17,20 +20,29 @@ const sketch = () => {
 
         context.save();
         context.translate(x, y);
-        context.translate(w * -0.5, h * -0.5);
-
         context.strokeStyle = "black";
-
-        context.beginPath();
-        context.moveTo(0, 0);
-        context.lineTo(w, 0);
-        context.lineTo(w, h);
-        context.lineTo(0, h);
-        context.closePath();
+        drowSkewedRect({ context });
         context.stroke();
-
         context.restore();
     };
+};
+
+const drowSkewedRect = ({ context, w = 600, h = 200, degrees = 45 }) => {
+    const angle = math.degToRad(degrees);
+    const rx = w * Math.cos(angle);
+    const ry = w * Math.sin(angle);
+    context.save();
+
+    context.translate(rx * -0.5, (ry + h) * -0.5);
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(rx, ry);
+    context.lineTo(rx, ry + h);
+    context.lineTo(0, h);
+    context.closePath();
+    context.stroke();
+
+    context.restore();
 };
 
 canvasSketch(sketch, settings);
