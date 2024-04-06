2bea8d4da193397917ee919e55d4f5ffcf2e782e
azakharenka <anton.zaharenko@gmail.com>
2024-04-06 16:55:32 +0300

    Adding Colors

--- main.js
+++ main.js
@@ -1,39 +1,59 @@
 const canvasSketch = require("canvas-sketch");
 const math = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
+const Color = require("canvas-sketch-util/color");
+const risoColors = require("riso-colors");
 
 const settings = {
     dimensions: [1080, 1080],
-    animate: true,
 };
 
 const sketch = ({ context, width, height }) => {
-    let x, y, w, h;
-    const num = 20;
-    const degrees = 50;
-
+    let x, y, w, h, fill, stroke, blend;
+    const num = 30;
+    const degrees = -40;
     const rects = [];
+    const bgColor = random.pick(risoColors).hex;
 
     for (let i = 0; i < num; i++) {
         x = random.range(0, width);
         y = random.range(0, height);
         w = random.range(200, 600);
         h = random.range(40, 200);
-        rects.push({ x, y, w, h });
+
+        fill = random.pick(risoColors).hex;
+        stroke = random.pick(risoColors).hex;
+        blend = random.value() > 0.5 ? "overlay" : "source-over";
+
+        rects.push({ x, y, w, h, fill, stroke, blend });
     }
 
-    console.log(rects);
-
     return ({ context, width, height }) => {
-        context.fillStyle = "white";
+        context.fillStyle = bgColor;
         context.fillRect(0, 0, width, height);
-
         rects.forEach((rect) => {
-            const { x, y, w, h } = rect;
+            const { x, y, w, h, fill, stroke, blend } = rect;
+            let shadowColor;
             context.save();
             context.translate(x, y);
-            context.strokeStyle = "black";
+            context.strokeStyle = stroke;
+            context.fillStyle = fill;
+            context.lineWidth = 10;
+            context.globalCompositeOperation = blend;
+
             drowSkewedRect({ context, w, h, degrees });
+
+            shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+            shadowColor.rgba[3] = 0.5;
+            context.shadowColor = Color.style(shadowColor.rgba);
+            context.shadowOffsetX = -10;
+            context.shadowOffsetY = 20;
+            context.stroke();
+            context.fill();
+
+            context.globalCompositeOperation = "source-over";
+            context.lineWidth = 2;
+            context.strokeStyle = "black";
             context.stroke();
             context.restore();
         });
