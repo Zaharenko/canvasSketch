6608868fb4c16a005d94cfbde4e073d82ab9155a
azakharenka <anton.zaharenko@gmail.com>
2024-04-08 01:54:13 +0300

    audio play

--- Audio/main.js
+++ Audio/main.js
@@ -1,14 +1,45 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+    dimensions: [1080, 1080],
 };
+
+let audio;
+let isPlaying = false;
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-  };
+    audio = document.createElement("audio");
+    audio.src = "song/Deja-Vu.mp3";
+
+    return ({ context, width, height }) => {
+        // Clear the canvas
+        context.clearRect(0, 0, width, height);
+
+        if (!isPlaying) {
+            context.fillStyle = "white";
+            context.fillRect(0, 0, width, height);
+
+            context.fillStyle = "green";
+            context.beginPath();
+            context.moveTo(width / 2 - 30, height / 2 - 50);
+            context.lineTo(width / 2 + 30, height / 2);
+            context.lineTo(width / 2 - 30, height / 2 + 50);
+            context.fill();
+        }
+    };
 };
 
+const addEventListener = () => {
+    window.addEventListener("mousedown", () => {
+        if (!isPlaying && audio.paused) {
+            audio.play();
+            isPlaying = true;
+        } else {
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+};
+
+addEventListener();
 canvasSketch(sketch, settings);
